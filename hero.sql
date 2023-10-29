d63e0e5e0c52281ba75b164cb5b4ece236ebcc45
yakisoba25 <isonj26@gmail.com>
2023-10-29 16:44:35 +0800

    feat added select active players

--- hero.sql
+++ hero.sql
@@ -1,6 +1,4 @@
-UPDATE public.hero
-SET is_active = false
-WHERE hero_id = 1;
- --- Delete the item associated with hero 1.*/
- DELETE FROM public.heroitem
- WHERE hero_id = 1;
+SELECT p.player_name, h.hero_name
+FROM public.player p
+JOIN public.hero h ON p.hero_id = h.hero_id
+WHERE h.is_active + true;
